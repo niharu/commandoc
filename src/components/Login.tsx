7542dd7a2bda02abf229667927158c58c5e852de
Ryo Nishimura <wv.nsmr@gmail.com>
2022-02-01 13:55:16 +0900

    commit in progress

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -6,15 +6,11 @@ import { SignInScreen } from "./SignInScreen";
 import firebase from "firebase/compat/app";
 import { firebaseConfig } from "../config/config"
 import 'firebase/compat/auth';
-import { GithubAuthProvider } from "firebase/auth";
-
-// const provider = new GithubAuthProvider();
 
 const uiConfig = {
   signInFlow: 'popup',
   // signInSuccessUrl: "/",
   signInOptions: [
-    // firebase.auth.GoogleAuthProvider.PROVIDER_ID,
     firebase.auth.GithubAuthProvider.PROVIDER_ID,
     firebase.auth.EmailAuthProvider.PROVIDER_ID
   ],
